59d9569d814222051ae7ec6f36cc35eba11aa0d6
Syalcindag <40965702+Syalcindag@users.noreply.github.com>
2020-09-22 11:11:42 +0300

    ngOnInit() function added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-using-trackby';
+  users: any[];
+  constructor(private httpClient: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<any[]>('https://jsonplaceholder.typicode.com/users').subscribe(data => {
+      this.users = data;
+    })
+  }
 }
